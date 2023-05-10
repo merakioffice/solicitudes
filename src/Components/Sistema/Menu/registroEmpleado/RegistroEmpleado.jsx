e17bae306a0229360927cb8634f65fba0191208b
Graciel69 <cesar95farfan@gmail.com>
2023-05-10 15:02:56 -0400

    feat: :art: fix table

--- src/Components/Sistema/Menu/registroEmpleado/RegistroEmpleado.jsx
+++ src/Components/Sistema/Menu/registroEmpleado/RegistroEmpleado.jsx
@@ -576,14 +576,14 @@ const RegistroEmpleado = ({isDarkMode}) => {
   const header = (
     <div className='flex flex-column md:flex-row md:justify-content-between md:align-items-center'>
       <h5 className='m-0'>Lista de Empleados</h5>
-{/*       <Dropdown
+      <Dropdown
         id='state'
         value={selectedCity1}
         options={cities}
         onChange={onCityChange}
         optionLabel='name'
         placeholder='Seleccionar'
-      /> */}
+      />
 {/*       <span className='block mt-2 md:mt-0 p-input-icon-left'>
         <i className='pi pi-search' />
         <InputText
